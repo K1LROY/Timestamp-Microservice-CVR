9c1ed4500de1a23d3bb1e8cfe5b06fb83833033c
Supatarchai <Luck-y_se7en@hotmail.com>
2022-02-15 02:02:50 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({ unix: now.valueOf(), utc: now.toUTCString() });
+});
+
+app.get("/api/:date", (req, res) => {
+  let inputDate;
+  if (!/\D/.test(req.params.date)) {
+    inputDate = new Date(Number(req.params.date));
+  } else {
+    inputDate = new Date(req.params.date);
+  }
+  if (inputDate.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+  res.json({ unix: inputDate.getTime(), utc: inputDate.toUTCString() });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
